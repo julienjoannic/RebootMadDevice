e338e0f0bdf3fc48dabb26925c885b003c2d3bbf
Joannic Julien <julien.joannic@sig-ge.ch>
2024-01-15 21:10:18 +0100

    Added -t option to restartATLAS.sh so it works properly

--- restartATLAS.sh
+++ restartATLAS.sh
@@ -5,7 +5,7 @@ ADBLOC=$1
 DEVICELOC=$2
 
 # ADB command to restart ATLAS
-$ADBLOC -s $DEVICELOC shell "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas && am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
+$ADBLOC -s $DEVICELOC shell -t "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas && am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
 RETURNCODE=$?
 
 # Returncode
